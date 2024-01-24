8d5e732b2611506cfa45fd8be489f159abcbd0c9
igoreduc340 <igoreducar740@gmail.com>
2024-01-24 19:06:10 -0300

    Criação do model Category,e ligando à Foreign Key em Contact

--- contact/admin.py
+++ contact/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from contact.models import Contact
+from contact.models import Contact,Category
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ("id","first_name","last_name","phone",)
     ordering = ("-id",)
     search_fields = ("id",)
+    
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    ordering = ("id",)
     
--- contact/models.py
+++ contact/models.py
@@ -3,6 +3,13 @@ from django.utils import timezone
 
 # Create your models here.
 
+class Category(models.Model):
+
+    name = models.CharField(max_length = 50)
+    
+    def __str__(self) -> str:
+        return f"{self.name}"
+
 
 
 class Contact(models.Model):
@@ -15,6 +22,10 @@ class Contact(models.Model):
     description = models.TextField(blank = True)
     show = models.BooleanField(default = True)
     picture = models.ImageField(blank = True,upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(Category,
+                                 on_delete = models.SET_NULL,
+                                 blank = True ,null = True,
+                                 )
     
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
